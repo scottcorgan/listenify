578a06ff3fffe103a9b1fb0c56deaa1802bf4b99
Scott Corgan <scottcorgan@gmail.com>
2014-01-29 10:54:40 -0800

    Build

--- dist/prominent.js
+++ dist/prominent.js
@@ -1,21 +1,27 @@
 !function(e){if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else{var n;"undefined"!=typeof window?n=window:"undefined"!=typeof global?n=global:"undefined"!=typeof self&&(n=self),n.prominent=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 var Promise = _dereq_('promise');
 var element = _dereq_('tiny-element');
+var delegate = _dereq_('tiny-delegate');
 
-var prominent = function (selector, evt) {
+var prominent = function (selector, evt, options) {
+  options = options || {};
+  
   return new Promise(function (resolve, reject) {
     var el = element(selector);
-    
     if (!el) return reject();
     
-    el.addEventListener(evt, function (e) {
-      resolve(e);
-    });
+    if (options.delegate) {
+      var bind = delegate(options.delegate)
+      bind(selector, evt, resolve);
+    }
+    else{
+      el.addEventListener(evt, resolve);
+    }
   });
 };
 
 module.exports = prominent;
-},{"promise":4,"tiny-element":6}],2:[function(_dereq_,module,exports){
+},{"promise":4,"tiny-delegate":6,"tiny-element":9}],2:[function(_dereq_,module,exports){
 // shim for using process in browser
 
 var process = module.exports = {};
@@ -282,6 +288,84 @@ if (typeof setImmediate === 'function') { // IE >= 10 & node.js >= 0.10
 }
 }).call(this,_dereq_("/Users/scott/www/modules/prominent/node_modules/browserify/node_modules/insert-module-globals/node_modules/process/browser.js"))
 },{"/Users/scott/www/modules/prominent/node_modules/browserify/node_modules/insert-module-globals/node_modules/process/browser.js":2}],6:[function(_dereq_,module,exports){
+var element = _dereq_('tiny-element');
+var closest = _dereq_('closest');
+
+var delegate = function (parent) {
+  return bind(parent);
+};
+
+var bind = function (parent) {
+  return function (selector, evt, callback) {
+    var parentEl = element(parent)
+    
+    if (!parentEl) return;
+    
+    parentEl.addEventListener(evt, function (e) {
+      var target = e.target || e.srcElement;
+      
+      e.delegateTarget = closest(target, selector, true);
+      if (e.delegateTarget) callback.call(parentEl, e);
+    });
+    
+  }
+};
+
+module.exports = delegate;
+},{"closest":7,"tiny-element":9}],7:[function(_dereq_,module,exports){
+var matches = _dereq_('matches-selector')
+
+module.exports = function (element, selector, checkYoSelf) {
+  var parent = checkYoSelf ? element : element.parentNode
+
+  while (parent && parent !== document) {
+    if (matches(parent, selector)) return parent;
+    parent = parent.parentNode
+  }
+}
+
+},{"matches-selector":8}],8:[function(_dereq_,module,exports){
+
+/**
+ * Element prototype.
+ */
+
+var proto = Element.prototype;
+
+/**
+ * Vendor function.
+ */
+
+var vendor = proto.matchesSelector
+  || proto.webkitMatchesSelector
+  || proto.mozMatchesSelector
+  || proto.msMatchesSelector
+  || proto.oMatchesSelector;
+
+/**
+ * Expose `match()`.
+ */
+
+module.exports = match;
+
+/**
+ * Match `el` to `selector`.
+ *
+ * @param {Element} el
+ * @param {String} selector
+ * @return {Boolean}
+ * @api public
+ */
+
+function match(el, selector) {
+  if (vendor) return vendor.call(el, selector);
+  var nodes = el.parentNode.querySelectorAll(selector);
+  for (var i = 0; i < nodes.length; ++i) {
+    if (nodes[i] == el) return true;
+  }
+  return false;
+}
+},{}],9:[function(_dereq_,module,exports){
 var slice = [].slice;
 var ctx = document;
 
